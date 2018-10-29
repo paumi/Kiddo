b4b93105d4b21c0d91c4312291f4a3e2f0f2e903
paumi <33665258+paumi@users.noreply.github.com>
2018-10-29 23:02:43 +0100

    0.1
    Proyecto Base del Juego

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    GameObject Kiddo;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        Kiddo = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position + Kiddo.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Kiddo.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/jumpOnEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpOnEnemy : MonoBehaviour {
+
+    public Rigidbody2D rb2d;
+    float jumpForce;
+    Collider collision;
+    // Use this for initialization
+    void Start () {
+        jumpForce = GameObject.FindGameObjectWithTag("Player").GetComponent<playerMovement>().jumpForce;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb2d.AddForce(Vector2.up * jumpForce*1.5f, ForceMode2D.Impulse);
+        Destroy(transform.parent.gameObject);
+        Destroy(gameObject);
+    }
+}
