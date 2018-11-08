23443fad920a58243e05be0dae971c371658375c
Esteban5555 <al361641@uji.es>
2018-11-08 16:05:13 +0100

    Mécanicas de movimiento más pulidas
    Arreglados varios bugs con las animaciones y el movimiento del protagonista y ajustada la gravedad.

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -36,7 +36,7 @@ public class playerController : MonoBehaviour {
 		
 	}
 
-    //Se llama cada frame y se utiliza para manejar las colisiones
+    //Es recomendable para hacer cambios en el rigidbody
 
     void FixedUpdate()
     {
@@ -45,21 +45,22 @@ public class playerController : MonoBehaviour {
 
         moveInputH = Input.GetAxisRaw("Horizontal");
         moveInputV = Input.GetAxis("Vertical");
-        
+
 
         //comprobamos si toca los limites de muerte
 
-        isDead = Physics2D.OverlapCircle(groundCheck.position, checkRadius, boundsD);
+        //isDead = Physics2D.OverlapCircle(groundCheck.position, checkRadius, boundsD);
 
-        if (isDead)
-        {
-            Destroy(gameObject);
-        }
+        //if (isDead)
+        //{
+        //Destroy(gameObject);
+        //}
 
         //Si no está agachado kiddo no puede moverse
 
-        if(!(moveInputV < 0))
-        { 
+
+        if (!(moveInputV < 0))
+        {
             //Movimiento en el eje horizontal
 
             rb.velocity = new Vector2(moveInputH * speed, rb.velocity.y);
@@ -68,45 +69,44 @@ public class playerController : MonoBehaviour {
 
             if (moveInputH > 0 && facingRight == false)
             {
-            Flip();
+                Flip();
             }
             else if (moveInputH < 0 && facingRight == true) { Flip(); }
         }
-        //cambiamos la variable Speed (ver Animator) cambiar la animación
+        else {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        //cambiamos la variable Speed (ver Animator) para cambiar la animación
 
         animator.SetFloat("Speed", Mathf.Abs(moveInputH * speed));
-    }
-
-    // Se llama cada frame
-
-    private void Update()
-    {
 
         //Se comprueba que está en el suelo
 
-        if (isGrounded)
-        {
-            //cambiamos la variable Speed (ver Animator) cambiar la animación
+    }
 
-            animator.SetBool("Jumping", false);
+    
 
-            //Restablecemos el numero de saltos que se pueden dar antes de tocar el suelo
-
-            extraJump = extraJumpValue;
-        }
-        else
-        {
-            //Si no se está en el suelo la animación de saltar se ejecuta
-
-            animator.SetBool("Jumping", true);
-        }
-
+    private void Update()
+    {
         //se maneja el salto 
 
-        if(extraJump > 0 && (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")))
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")))
         {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJump--;
+
+            if (isGrounded)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+            else
+            {
+                if (extraJump > 0)
+                {
+                    rb.velocity = Vector2.up * jumpForce;
+                    extraJump--;
+                }
+
+            }
+
         }
 
         //Se maneja si se agacha o no
@@ -116,7 +116,23 @@ public class playerController : MonoBehaviour {
             animator.SetBool("Down", true);
         }
         else { animator.SetBool("Down", false); }
+
+        if (isGrounded)
+        {
+
+            //Restablecemos el numero de saltos que se pueden dar antes de tocar el suelo
+            animator.SetBool("Jumping", false);
+            extraJump = extraJumpValue;
+        }
+        else
+        {
+            //Si no se está en el suelo la animación de saltar se ejecuta
+
+            animator.SetBool("Jumping", true);
+        }
     }
+
+
 
     //Función para cmbiar la dirección de los sprites
 
