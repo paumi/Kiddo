4432614cb0087e804b9c41192ae41dce3f457568
paumi <33665258+paumi@users.noreply.github.com>
2018-11-11 19:43:20 +0100

    Añadido Obstaculo
    Añadido el obstaculo de la papelera

---
+++ Assets/Images/destroySmell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroySmell : MonoBehaviour {
+
+    float time = 0;
+    Collider2D collision;
+    GameObject lePlayer;
+    public bool col = false;
+
+	// Use this for initialization
+	void Start () {
+        lePlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 1.5f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
