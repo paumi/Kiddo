b5fc058b9944a27e8755c80abd56d970640f57cc
pauborrell <pauborrellegea@gmail.com>
2018-11-12 16:30:48 +0100

    Tutorial - Distribución - Cámara - Fondo
    He implementado el tutorial del juego, las pizarras. Además he mejorado la distribución que no estaba del todo bien colocado. La cámara ya no sigue el salto de Kiddo. Y creación dle fondo para las ventanas.

--- Assets/scripts/Camera_Movement_Pau.cs
+++ Assets/scripts/Camera_Movement_Pau.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraMovement : MonoBehaviour {
+public class Camera_Movement_Pau : MonoBehaviour {
 
     public Transform target;
     public float softness;
     private Vector3 desfase;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         softness = 5f;
         desfase = transform.position - target.position;
-	}
+    }
 
     private void FixedUpdate()
     {
         Vector3 targetPosition = target.position + desfase;
         transform.position = Vector3.Lerp(transform.position, targetPosition, softness * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, 8.8f, -10f);
 
     }
 }
