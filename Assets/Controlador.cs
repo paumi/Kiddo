c59f64e642994379884b74bfe8d334478b87643c
mavivimat <al285841@uji.es>
2018-11-12 12:15:58 +0100

    menu y plataformas
    Se ha implementado el menu y las plataformas nivel inicial

---
+++ Assets/Controlador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour {
+
+	public void CambiarEscena(string nombre)
+    {
+        print("Cambia a escena" + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+    public void Salir()
+    {
+        print("Saliendo del juego");
+        Application.Quit();
+    }
+}
