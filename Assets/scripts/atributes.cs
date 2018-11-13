eab38e0a2c8dfadd2db7d1bde21f20bfcc935dd5
paumi <33665258+paumi@users.noreply.github.com>
2018-11-13 22:37:53 +0100

    Acabada Papelera
    Añadido script para que la papelera mate a "Kiddo"

---
+++ Assets/scripts/atributes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atributes : MonoBehaviour {
+
+    public int life = 1;
+    GameObject spawn;
+
+	// Use this for initialization
+	void Start () {
+        spawn = GameObject.FindGameObjectWithTag("Spawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!CheckLife())
+            ReSpawn();
+	}
+
+    bool CheckLife()
+    {
+        if (life == 0)
+        {
+            life = 1;
+            return false;
+        }
+            
+
+        else
+            return true;
+    }
+
+    void ReSpawn()
+    {
+        transform.position = spawn.transform.position;
+    }
+}
