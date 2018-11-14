dc40e27879c9405ba9ef1c392b130c25eaf5e2d9
pauborrell <pauborrellegea@gmail.com>
2018-11-14 13:22:15 +0100

    NPC mov

---
+++ Assets/scripts/Enemy_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour {
+
+    public float maxSpeed = 1f;
+    public float speed = 1f;
+    public Animator animator;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.AddForce(Vector2.right * speed);
+        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+
+        if (rb2d.velocity.x > -0.01f && rb2d.velocity.x < 0.01f){
+            speed = -speed;
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+
+        }
+
+        if (speed > 0) {
+
+            transform.localScale = new Vector3(2.356135f, 2.286112f, 1.354422f);
+
+        }
+        else if (speed < 0) {
+            transform.localScale = new Vector3(-2.356135f, 2.286112f, 1.354422f);
+
+        }
+
+    }
+
+
+}
---
+++ Assets/scripts/kill_player_bully.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill_player_bully : MonoBehaviour
+{
+
+    GameObject Kiddo;
+    // Use this for initialization
+    void Start()
+    {
+        Kiddo = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Kiddo.GetComponent<atributes>().life = 0;
+    }
+}
