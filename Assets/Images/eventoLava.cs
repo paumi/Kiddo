45070d2772dcfed98e9b1c90cbd6929c19412d7d
paumi <33665258+paumi@users.noreply.github.com>
2018-11-22 11:17:18 +0100

    Evento Lava
    Empezado el evento de la lava, por ahora aparecen muros invisibles que impiden el movimiento del jugador y sale un bocadillo con instrucciones.

---
+++ Assets/Images/eventoLava.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventoLava : MonoBehaviour {
+
+    public GameObject wallLeft, wallRight;
+    public GameObject msg;
+    bool caged = false;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (caged)
+        {
+            Instantiate(msg);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        WallController(true);
+        caged = true;
+    }
+
+    private void WallController(bool active)
+    {
+        wallLeft.SetActive(active);
+        wallRight.SetActive(active);
+    }
+}
