7034a573a34ad5da7d86c00326c25bdc77706e4a
pablomorro <al361654@uji.es>
2018-11-24 17:02:47 +0100

    He añadido nubes

---
+++ Assets/scripts/ScriptsPlatforms/DesapareceNube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesapareceNube : MonoBehaviour {
+
+    public float duracion = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    IEnumerator DestruirNube() {
+
+        yield return new WaitForSeconds(duracion);
+
+        GetComponent<Collider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duracion);
+
+        GetComponent<Collider2D>().enabled = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            StartCoroutine(DestruirNube());
+        }
+
+    }
+}
