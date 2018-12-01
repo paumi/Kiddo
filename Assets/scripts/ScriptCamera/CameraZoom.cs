bb339395cee2329797c533709ede6ea5f03f453f
pablomorro <al361654@uji.es>
2018-12-01 13:03:16 +0100

    Añadido parallax al nivel dos y cambiado el funcionamiento de los fondos, añadido zoom a las pizarras
    Hay un problema con las pizarras y es que la letra se distorsiona cuando te mueves con lo que al realizar el zoom el efecto se hace más notorio

---
+++ Assets/scripts/ScriptCamera/CameraZoom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+    private float currentCameraSize; //La inicial
+    private float zoomedCameraSize; //La cantidad de zoom que queremos
+    private float duracionZoom; //La duracion
+    private bool zoom; //Si ya ha hecho el zoom o no
+
+	// Use this for initialization
+	void Start () {
+
+        currentCameraSize = UnityEngine.Camera.main.orthographicSize;
+        zoomedCameraSize = 7f;
+        duracionZoom = 3f;
+        zoom = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !zoom)
+        {
+            UnityEngine.Camera.main.orthographicSize = zoomedCameraSize;
+
+            StartCoroutine(HacerZoom());
+
+        }           
+       
+    }
+
+    IEnumerator HacerZoom()
+    {
+        zoom = true;
+        yield return new WaitForSeconds(duracionZoom);
+        UnityEngine.Camera.main.orthographicSize = currentCameraSize;
+    }
+
+
+}
