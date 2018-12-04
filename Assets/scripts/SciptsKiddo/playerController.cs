66733fa8b1c530c96fe539a9bba5fd97df6fcdcd
Esteban5555 <al361641@uji.es>
2018-12-04 12:59:07 +0100

    pelota final

--- Assets/scripts/SciptsKiddo/playerController.cs
+++ Assets/scripts/SciptsKiddo/playerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour {
 
@@ -304,6 +305,14 @@ public class playerController : MonoBehaviour {
             Debug.Log("extraJump");
             extraJumpValue = 1;
  
+
+        }
+
+        if (col.gameObject.tag == "pelota")
+        {
+            Debug.Log("pelota");
+            SceneManager.LoadScene("Menu");
+
 
         }
     }
